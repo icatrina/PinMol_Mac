58dd977a3d1acc060accb401deec16382468adce
Irina Catrina <iecatrina@gmail.com>
2017-07-14 18:54:33 -0400

    Update pinmol_mac.py

--- pinmol_mac.py
+++ pinmol_mac.py
@@ -10,7 +10,7 @@ print("\n"*5)
 print('PinMol  Copyright (C) 2017  Irina E. Catrina' + '\n'+
 'This program comes with ABSOLUTELY NO WARRANTY;'  + '\n' + 
 'This is free software, and you are welcome to redistribute it' + '\n'+
-'under certain conditions; for details read the included GNU_GPL.txt file' + '\n')
+'under certain conditions; for details please read the included GNU_GPL.txt file' + '\n')
 undscr = "->"*30
 print(undscr)
 print("\n"+"WARNING: Previous files will be overwritten!  Save them in a "+"\n"+"different location than the current file, or rename them to "+"\n"+"ensure they are not misused (e.g. use probes from a different target)."+"\n")
