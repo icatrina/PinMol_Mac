70057f40b2ff3f80f36ce793d8b4e38699a7bb47
Irina Catrina <iecatrina@gmail.com>
2017-07-14 18:42:41 -0400

    Update pinmol_mac.py

--- pinmol_mac.py
+++ pinmol_mac.py
@@ -7,6 +7,10 @@ from Bio.SeqUtils import MeltingTemp as mt
 import subprocess
 from Bio.Blast import NCBIXML
 print("\n"*5)
+print('PinMol  Copyright (C) 2017  Irina E. Catrina' + '\n'+
+'This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.' + '\n'+
+'This is free software, and you are welcome to redistribute it' + '\n'+
+'under certain conditions; type' + '\`'+'show c'+'\''+ 'for details.'+ '\n')
 undscr = "->"*30
 print(undscr)
 print("\n"+"WARNING: Previous files will be overwritten!  Save them in a "+"\n"+"different location than the current file, or rename them to "+"\n"+"ensure they are not misused (e.g. use probes from a different target)."+"\n")
