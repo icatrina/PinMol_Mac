bb3cc9ef35149a0f68c27e90199ad2a14247d544
Irina E. Catrina <iecatrina@gmail.com>
2017-07-14 18:59:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='PinMol',
-      version='0.1',
+      version='1.0',
       description='Find molecular beacons for live cell imaging of mRNAs',
       url='https://github.com/icatrina/PinMol_Mac',
       author='Irina Catrina',
